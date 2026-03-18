969f3aefcdc01d66afbc722786fa78b1a19b54e3
arch1e <121162902+archie0732@users.noreply.github.com>
2026-03-18 15:25:44 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,25 @@ const autoLogin = async () => {
     throw new Error("Lost .env file");
   }
 
-  const response = await fetch(path, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, password }),
-  });
+  try {
+    const response = await fetch(path, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-  if (!response.ok) {
-    throw new Error(`login fail: ${response.status}`);
+    if (!response.ok) {
+      throw new Error(`login fail: ${response.status}`);
+    }
+
+    console.log("Login successful!");
   }
-
-  console.log("Login successful!");
+  catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
 
 autoLogin();
